6aeadd14fa04869f73c8cea33715dd1fa586391c
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-04-13 09:54:31 -0400

    it doesnt work

---
+++ PIDspinny.py
@@ -0,0 +1,70 @@
+import board
+from lcd.lcd import LCD
+from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
+from time import sleep
+import rotaryio
+from digitalio import DigitalInOut, Direction, Pull
+import digitalio
+# get and i2c object
+i2c = board.I2C()
+
+# some LCDs are 0x3f... some are 0x27.
+lcd = LCD(I2CPCF8574Interface(i2c, 0x27), num_rows=2, num_cols=16)
+#screen test
+lcd.print("hey")
+sleep(1)
+lcd.clear()
+#setting up stuff
+encoder = rotaryio.IncrementalEncoder(board.D2, board.D3, divisor=2)
+button = digitalio.DigitalInOut(board.D4)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+
+kP=1
+kI=0
+kD=0
+encoder.position=0
+menu=1
+last_position = 0
+Setpoint=45
+dt=5
+
+while True:
+    position = encoder.position
+    if position == (last_position+1):
+        menu+=1
+    elif position == (last_position-1):
+        menu-=1
+    if menu<0:
+        menu=4
+    elif menu>4:
+        menu=1
+    Previous_error=Error
+    Error = Setpoint - MV
+    Integral = Integral + (Error*dt)
+    Derivative = (Error - Previous_error)/dt
+    Drive = (Error*kP) + (Integral*kI) + Derivative*kD
+#checks which light is selected
+    if menu == 1:
+        lcd.print("kP = "+str(kP))
+    if menu == 2:
+        lcd.print("kI = "+str(kI))
+    if menu == 3:
+        lcd.print("kD = "+str(kD))
+    if menu == 4:
+        lcd.print("kP="+str(kP)+"|kI="+str(kI)+"|kD="+str(kD))
+    #activates light if button is down
+    if not button.value:
+        if menu == 1:
+            kP+=1
+        if menu == 2:
+            kI+=1
+        if menu == 3:
+            kD+=1
+        sleep(2)
+    last_position = position
+    sleep(.2)
+    lcd.clear()
+
+
