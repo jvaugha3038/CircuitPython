2addf4eb90550b07cd4933f139080ead183f6999
jvaugha3038 <jvaugha3038@charlottesvilleschools.org>
2023-04-18 09:03:34 -0400

    a

--- PIDspinny.py
+++ PIDspinny.py
@@ -3,7 +3,6 @@ from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 from time import sleep
 import rotaryio
-from digitalio import DigitalInOut, Direction, Pull
 import digitalio
 import adafruit_mpu6050
 # get and i2c object
@@ -25,7 +24,6 @@ button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
 
 deg=0
-ack=1
 kP=1
 kI=0
 kD=0
@@ -36,49 +34,44 @@ Setpoint=45
 dt=5
 prev=0
 
-
 while True:
     position = encoder.position
     sleep(.05)
     prev=deg
     deg=(round(float(mpu.gyro[0])+0.038, 1)*(.05)*(180/3.14))+prev
-
-    
-
-    if ack==2:
-        if position == (last_position+1):
-            menu+=1
-        elif position == (last_position-1):
-            menu-=1
-        if menu<0:
-            menu=4
-        elif menu>4:
-            menu=1
-        Previous_error=Error
-        Error = Setpoint - deg
-        Integral = Integral + (Error*dt)
-        Derivative = (Error - Previous_error)/dt
-        Drive = (Error*kP) + (Integral*kI) + Derivative*kD
-    #checks which light is selected
+    if position == (last_position+1):
+        menu+=1
+    elif position == (last_position-1):
+        menu-=1
+    if menu<0:
+        menu=4
+    elif menu>4:
+        menu=1
+    Previous_error=Error
+    Error = Setpoint - deg
+    Integral = Integral + (Error*dt)
+    Derivative = (Error - Previous_error)/dt
+    Drive = (Error*kP) + (Integral*kI) + Derivative*kD
+#checks which light is selected
+    if menu == 1:
+        lcd.print("kP = "+str(kP))
+    if menu == 2:
+        lcd.print("kI = "+str(kI))
+    if menu == 3:
+        lcd.print("kD = "+str(kD))
+    if menu == 4:
+        lcd.print("kP="+str(kP)+"|kI="+str(kI)+"|kD="+str(kD))
+    #activates light if button is down
+    if not button.value:
         if menu == 1:
-            lcd.print("kP = "+str(kP))
+            kP+=1
         if menu == 2:
-            lcd.print("kI = "+str(kI))
+            kI+=1
         if menu == 3:
-            lcd.print("kD = "+str(kD))
-        if menu == 4:
-            lcd.print("kP="+str(kP)+"|kI="+str(kI)+"|kD="+str(kD))
-        #activates light if button is down
-        if not button.value:
-            if menu == 1:
-                kP+=1
-            if menu == 2:
-                kI+=1
-            if menu == 3:
-                kD+=1
-            sleep(.2)
-        last_position = position
+            kD+=1
         sleep(.2)
-        lcd.clear()
+    last_position = position
+    sleep(.2)
+    lcd.clear()
 
 
